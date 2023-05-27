603849e65888c40a300dd79f43085cdaa99c91c2
rezwan <rezwanshahriorrahin@gmail.com>
2023-05-27 15:14:50 +0600

    registered 'Identity' service in project Program file

--- MyProfile/Program.cs
+++ MyProfile/Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MyProfile.Context;
+using MyProfile.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,9 @@ builder.Services.AddControllersWithViews();
 // Register DbContext
 var connectionString = builder.Configuration.GetConnectionString("MyProfileDbConnection");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+// Identity Service
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
 
 var app = builder.Build();
 

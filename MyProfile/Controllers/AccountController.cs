1076c7186711e0c4914812868211316b460dc3fe
rezwan <rezwanshahriorrahin@gmail.com>
2023-10-22 13:16:20 +0600

    decorated 'AccountController' class with 'AllowAnonymous' attribute

--- MyProfile/Controllers/AccountController.cs
+++ MyProfile/Controllers/AccountController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MyProfile.Controllers
 {
+	[AllowAnonymous]
 	public class AccountController : Controller
 	{
 

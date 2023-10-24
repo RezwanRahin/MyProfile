f1255787ee6350a6bcd5e1c8da46c661529dffe3
rezwan <rezwanshahriorrahin@gmail.com>
2023-10-24 16:02:52 +0600

    added a method named 'ProcessUploadedFile' to 'FileHelper' class

--- MyProfile/Extensions/FileHelper.cs
+++ MyProfile/Extensions/FileHelper.cs
@@ -2,6 +2,18 @@ namespace MyProfile.Extensions
 {
 	public static class FileHelper
 	{
-		
+		public static async Task<string> ProcessUploadedFile(this IFormFile photoFile, IWebHostEnvironment hostEnvironment)
+		{
+			var uploadsFolder = Path.Combine(hostEnvironment.WebRootPath, "images");
+			var uniqueFileName = Guid.NewGuid().ToString() + '_' + photoFile.FileName;
+			var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+			await using (var fileStream = new FileStream(filePath, FileMode.Create))
+			{
+				await photoFile.CopyToAsync(fileStream);
+			}
+
+			return uniqueFileName;
+		}
 	}
 }

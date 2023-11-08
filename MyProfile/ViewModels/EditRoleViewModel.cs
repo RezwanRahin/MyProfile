afb083147f38ec99f0eae74f3fdc260b9177ae51
rezwan <rezwanshahriorrahin@gmail.com>
2023-11-08 14:39:37 +0600

    created a ViewModel named 'EditRoleViewModel'

---
+++ MyProfile/ViewModels/EditRoleViewModel.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MyProfile.ViewModels
+{
+    public class EditRoleViewModel
+    {
+        public EditRoleViewModel()
+        {
+            Users = new List<string>();
+        }
+
+        public string Id { get; set; }
+
+        [Required]
+        public string RoleName { get; set; }
+
+        public List<string> Users { get; set; }
+    }
+}

30cbaff9aa27f4130171f5a1c431d638569e4763
rezwan <rezwanshahriorrahin@gmail.com>
2023-11-25 17:52:18 +0600

    added a HttpGet action method named 'EditUser' to 'AdministrationController' class

--- MyProfile/Controllers/AdministrationController.cs
+++ MyProfile/Controllers/AdministrationController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using MyProfile.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using MyProfile.Models;
 
@@ -27,6 +28,37 @@ namespace MyProfile.Controllers
 		{
 			var users = _userManager.Users;
 			return View(users);
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> EditUser(string id)
+		{
+			var user = await _userManager.FindByIdAsync(id);
+
+			if (user == null)
+			{
+				Response.StatusCode = 404;
+				ViewBag.ErrorMessage = $"User with Id = {id} cannot be found";
+				return View("NotFound");
+			}
+
+			var userClaims = await _userManager.GetClaimsAsync(user);
+			var userRoles = await _userManager.GetRolesAsync(user);
+
+			var model = new EditUserViewModel
+			{
+				Id = user.Id,
+				FirstName = user.FirstName,
+				LastName = user.LastName,
+				Email = user.Email,
+				UserName = user.UserName,
+				Gender = user.Gender,
+				DOB = user.DOB,
+				Claims = userClaims.Select(c => c.Value).ToList(),
+				Roles = userRoles
+			};
+
+			return View(model);
 		}
 	}
 }

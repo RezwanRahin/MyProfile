d9412b3fa25095da5e318a06a751c3b0697747e9
rezwan <rezwanshahriorrahin@gmail.com>
2023-12-03 17:50:26 +0600

    injected 'UserManager', 'SignInManager', and 'RoleManager' services via constructor injection in 'AdministrationController' class

--- MyProfile/Controllers/AdministrationController.cs
+++ MyProfile/Controllers/AdministrationController.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyProfile.Models;
 
 namespace MyProfile.Controllers
 {
 	public class AdministrationController : Controller
 	{
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly SignInManager<ApplicationUser> _signInManager;
+		private readonly RoleManager<IdentityRole> _roleManager;
+
+		public AdministrationController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager)
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+			_roleManager = roleManager;
+		}
+
 		public IActionResult Index()
 		{
 			return View();

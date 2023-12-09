c2b83c233f2ced4527ec30fb498b46a662fcb8ae
rezwan <rezwanshahriorrahin@gmail.com>
2023-12-09 19:13:16 +0600

    added a HttpGet action method named 'Edit' to 'AccountController' class

--- MyProfile/Controllers/AccountController.cs
+++ MyProfile/Controllers/AccountController.cs
@@ -172,5 +172,39 @@ namespace MyProfile.Controllers
 
 			return View(model);
 		}
+
+		[HttpGet]
+		[Authorize]
+		public async Task<IActionResult> Edit(string username)
+		{
+			var user = await _userManager.FindByNameAsync(username);
+			var signedInUser = await _userManager.GetUserAsync(User);
+
+			if (user != signedInUser)
+			{
+				return RedirectToAction("AccessDenied");
+			}
+
+			if (user == null)
+			{
+				Response.StatusCode = 404;
+				ViewBag.ErrorMessage = $"User with Username = {username} cannot be found";
+				return View("NotFound");
+			}
+
+			var model = new EditProfileViewModel
+			{
+				Id = user.Id,
+				FirstName = user.FirstName,
+				LastName = user.LastName,
+				Email = user.Email,
+				Username = user.UserName,
+				Gender = user.Gender,
+				DOB = user.DOB,
+				PhotoPath = user.PhotoPath
+			};
+
+			return View(model);
+		}
 	}
 }
